56f5cec4c9faabc5b58434900b55664b0a0a36a2
Romarjo Balukja <mbalukja1@gmail.com>
2020-12-12 16:19:12 +0100

    Refactor - Refactor sale component into function component

--- client/src/components/sales/Sale.js
+++ client/src/components/sales/Sale.js
@@ -10,78 +10,66 @@ import InvoiceModal from '../invoice/Invoice';
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 import tableColumns from '../mui-datatables/table.columns';
 
-class Sale extends Component {
-    static propTypes = {
-        getSales: PropTypes.func.isRequired,
-        getSaleById: PropTypes.func.isRequired,
-        updateSale: PropTypes.func.isRequired,
-        sale: PropTypes.object.isRequired,
-        isAuthenticated: PropTypes.bool,
-        isLoading: PropTypes.bool.isRequired,
-    };
-
-    componentWillMount() {
-        this._refreshSales();
-    }
-
-    onDeleteClick = (id) => {
+const SaleComponent = (props) => {
+    const onDeleteClick = (id) => {
         this.props.deleteSale(id);
     };
 
-    _refreshSales() {
-        this.props.getSales();
-    }
+    const _refreshSales = () => {
+        props.getSales();
+    };
 
-    render() {
-        const { sales } = this.props.sale;
-        const columns = tableColumns.saleColumns;
-        const data = [];
+    const { sales } = props.sale;
+    const columns = tableColumns.saleColumns;
+    const data = [];
 
-        sales.map(({ _id, clientName, total, date, invoiceData }) =>
-            data.push([
-                _id,
-                clientName,
-                total,
-                moment(date).calendar(),
-                <Button
-                    className="remove-btn mb-2"
-                    outline
-                    color="danger"
-                    size="sm"
-                    onClick={this.onDeleteClick.bind(this, _id)}
-                >
-                    Fshi
-                </Button>,
-                <InvoiceModal
-                    invoiceTitle="Shitje"
-                    invoiceType={1}
-                    invoiceData={invoiceData}
-                    client={clientName}
-                    transactionId={_id}
-                    refreshData={this._refreshSales.bind(this)}
-                />,
-            ]),
-        );
+    sales.map(({ _id, clientName, total, date, invoiceData }) =>
+        data.push([
+            _id,
+            clientName,
+            total,
+            moment(date).calendar(),
+            <Button className="remove-btn mb-2" outline color="danger" size="sm" onClick={() => onDeleteClick(_id)}>
+                Fshi
+            </Button>,
+            <InvoiceModal
+                invoiceTitle="Shitje"
+                invoiceType={1}
+                invoiceData={invoiceData}
+                client={clientName}
+                transactionId={_id}
+                refreshData={_refreshSales}
+            />,
+        ]),
+    );
 
-        const options = {
-            filterType: tableOptions.tableFilterTypes.DROPDOWN,
-            responsive: tableOptions.tableResponsiveness.STANDARD,
-            selectableRows: tableOptions.selectableRows.NONE,
-            isRowSelectable: function () {
-                return false;
-            },
-        };
+    const options = {
+        filterType: tableOptions.tableFilterTypes.DROPDOWN,
+        responsive: tableOptions.tableResponsiveness.STANDARD,
+        selectableRows: tableOptions.selectableRows.NONE,
+        isRowSelectable: function () {
+            return false;
+        },
+    };
 
-        return (
-            <div style={{ width: '100%' }}>
-                <InvoiceModal invoiceTitle="Shitje" invoiceType={1} />
-                {this.props.isAuthenticated && !this.props.isLoading ? (
-                    <MUIDataTable title={'Lista e Shitjeve'} data={data} columns={columns} options={options} />
-                ) : null}
-            </div>
-        );
-    }
-}
+    return (
+        <div style={{ width: '100%' }}>
+            <InvoiceModal invoiceTitle="Shitje" invoiceType={1} />
+            {props.isAuthenticated && !props.isLoading ? (
+                <MUIDataTable title={'Lista e Shitjeve'} data={data} columns={columns} options={options} />
+            ) : null}
+        </div>
+    );
+};
+
+SaleComponent.propTypes = {
+    getSales: PropTypes.func.isRequired,
+    getSaleById: PropTypes.func.isRequired,
+    updateSale: PropTypes.func.isRequired,
+    sale: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool,
+    isLoading: PropTypes.bool.isRequired,
+};
 
 const mapStateToProps = (state) => ({
     sale: state.sale,
@@ -89,4 +77,11 @@ const mapStateToProps = (state) => ({
     isLoading: state.sale.loading,
 });
 
-export default connect(mapStateToProps, { getSales, deleteSale, getSaleById, updateSale })(Sale);
+const mapDispatchToProps = {
+    getSales,
+    deleteSale,
+    getSaleById,
+    updateSale,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SaleComponent);
